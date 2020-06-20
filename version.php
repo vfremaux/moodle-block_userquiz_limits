f70d26563a47c5de79beaccf15a163a994ef93a7
valery <valery.fremaux@gmail.com>
2020-06-20 12:15:17 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2020011500; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018112800; // Requires this Moodle version.
+$plugin->requires  = 2019051100; // Requires this Moodle version.
 $plugin->component = 'block_userquiz_limits'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.6.0 (Build 2020011500)';
+$plugin->release = '3.7.0 (Build 2020011500)';
 $plugin->dependencies = array('quizaccess_usernumattempts' => 2016062300);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.6.0004';
+$plugin->codeincrement = '3.7.0004';
